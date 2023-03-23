ae9786790ca24629b2c2907b97440c87901a0f62
kinuj980 <kinuthiajohn980@gmail.com>
2023-03-23 19:00:44 +0300

    city,state

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,19 +1,20 @@
-
-
 #!/usr/bin/python3
-"""  lists all states from the database hbtn_0e_0_usa """
-import MySQLdb
-import sys
-
-
+""" Script that lists all cities from the database hbtn_0e_4_usa.
+"""
 if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
-                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
-    cur = db.cursor()
-    cur.execute("""SELECT cities.id, cities.name, states.name FROM
-                cities INNER JOIN states ON states.id=cities.state_id""")
-    rows = cur.fetchall()
+    import sys
+    import MySQLdb
+
+    serv = MySQLdb.connect(host="localhost",  port=3306,
+                           user=sys.argv[1], password=sys.argv[2],
+                           database=sys.argv[3])
+
+    c = serv.cursor()
+    c.execute("SELECT cities.id, cities.name, states.name\
+              FROM cities JOIN states ON cities.state_id = states.id\
+              ORDER BY id ASC")
+    rows = c.fetchall()
     for row in rows:
         print(row)
-    cur.close()
-    db.close()
+    c.close()
+    serv.close()
