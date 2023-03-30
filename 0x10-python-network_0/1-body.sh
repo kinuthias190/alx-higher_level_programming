303441ae8eda04a14056b255feadc555bccd7d71
kinuj980 <kinuthiajohn980@gmail.com>
2023-03-30 19:52:11 +0300

    network project

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
-#!/bin/bash                                                                   
-# a Bash script that takes in a URL, sends a GET request to the URL, and displays the body of the response                                                  
-if [ "$(curl -sLI "$1" -X GET | grep "200 OK" | cut -d' ' -f2)" = '200' ]; then curl -sL "$1"; fi
+#!/bin/bash
+# script that takes in a URL, sends a GET request to the URL, and displays the body of the response# script that takes in a URL, sends a request to that URL, and displays the size of the body of the response
+curl -sL "$1"
